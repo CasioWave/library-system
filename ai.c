ddcc3d5f2bd2e6c666620946bd2774ed5385e3d0
CasioWave <diptanuj.ds@gmail.com>
2024-11-13 15:17:58 +0530

    Incomplete preprocess

--- ai.c
+++ ai.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include "soundex.h"
 
 #define MAX_LINE_LENGTH 1000
 #define MAX_TOKENS 10000
@@ -102,37 +103,6 @@ void process_tokens(char* line, UniqueToken** tokens, int* count, int index) {
     }
 }
 
-char* soundex(const char* x, int m) {
-    char* hold = strong_sanitize(x);
-    int len = strlen(hold);
-    char* result = malloc(m + 1);
-    result[0] = toupper(hold[0]);
-    int r = 1;
-
-    for (int i = 1; i < len && r < m; i++) {
-        char code = '0';
-        switch (hold[i]) {
-            case 'b': case 'f': case 'p': case 'v': code = '1'; break;
-            case 'c': case 'g': case 'j': case 'k': case 'q': case 's': case 'x': case 'z': code = '2'; break;
-            case 'd': case 't': code = '3'; break;
-            case 'l': code = '4'; break;
-            case 'm': case 'n': code = '5'; break;
-            case 'r': code = '6'; break;
-        }
-        if (code != '0' && code != result[r-1]) {
-            result[r++] = code;
-        }
-    }
-
-    while (r < m) {
-        result[r++] = '0';
-    }
-
-    result[m] = '\0';
-    free(hold);
-    return result;
-}
-
 char* stitch(int* arr, int count) {
     char* result = malloc(count * 12);  // Assuming each number is at most 10 digits + 1 for '-' + 1 for '\0'
     result[0] = '\0';
@@ -186,7 +156,8 @@ int main() {
 
     for (int i = 0; i < un_t_count; i++) {
         char* indices = stitch(un_t[i].indices, un_t[i].count);
-        char* sdx = soundex(un_t[i].token, 5);
+        char* sdx = (char*) malloc(6*sizeof(char));;
+	soundex(un_t[i].token, sdx);
         fprintf(output, "%s,title,%s,%s\n", un_t[i].token, indices, sdx);
         free(indices);
         free(sdx);
