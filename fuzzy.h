84c87cc69ac838ae9c6fc418f8b5f3346bb57b41
CasioWave <diptanuj.ds@gmail.com>
2024-11-17 00:43:27 +0530

    I think I finally fixed search?

--- fuzzy.h
+++ fuzzy.h
@@ -4,12 +4,13 @@
 #include "search-utils.h"
 #include "soundex.h"
 #define LEVWEIGHT 10
-#define SOUNDWEIGHT 5
-#define MAXLEV 1 //Important - this integer controls how fuzzy it gets
+#define EXACT_BONUS 100
+#define SOUNDWEIGHT 0.5
+#define MAXLEV 0 //Important - this integer controls how fuzzy it gets
 #define SOUND_LEN 5
-#define SYN_PENALTY 2
+#define SYN_PENALTY 5
 #define MAXQUERY 20
 #define MAXRES 1000
 
-int fuzzy_search(char* query, char* dict_file, int**);
+int* fuzzy_search(char* query, char* dict_file);
 int read_line(FILE* file, char* ret);
